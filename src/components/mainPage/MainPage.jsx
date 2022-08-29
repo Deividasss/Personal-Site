aa5876b527605d15cdb35156afaac7976c610a8a
Deividasss <kasetukas123@gmail.com>
2022-08-29 12:07:56 +0300

    update

--- src/components/mainPage/MainPage.jsx
+++ src/components/mainPage/MainPage.jsx
@@ -10,19 +10,19 @@ const MainPage = () => {
         <>
             <div className="mainPage">
                 <div className="mainHeading">
-                    <h1>Hi, my <br /> name is <strong>Deividas</strong></h1>
-                    <p>I am an independent creative Front-End developer from <br /> Lithuania</p>
-                    <button className="readMoreBtn" onClick={() => navigate("About")}>Read more</button>
-                </div>
-                <div>
-                    <img className="mainImg" src={mono2}></img>
+                    <div className="headingText">
+                        <h1>Hi, my <br /> name is <strong>Deividas</strong></h1>
+                        <p>I am an independent creative Front-End developer from <br /> Lithuania</p>
+                        <button className="readMoreBtn" onClick={() => navigate("About")}>Read more</button>
+                    </div>
+                    <img src={mono2}></img>
                 </div>
                 <div className="projectHeading">
-                    <img className="mainImg" src={mono4}></img>
-                    <div>
+                    <div className="projectText">
                         <h1>You can find <br />my projects here</h1>
                         <button className="projectsBtn" onClick={() => navigate("projects")}>Projects</button>
                     </div>
+                    <img src={mono4}></img>
                 </div>
             </div>
         </>
