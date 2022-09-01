c8f64644b0eb527e8e5606c695dc18084a3394c1
Deividasss <kasetukas123@gmail.com>
2022-09-01 12:00:20 +0300

    update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,12 +11,7 @@ const About = () => {
                         <h1>Deividas <br /> Kašėta</h1>
                         <h3>Junior Web Developer</h3>
                         <p>Front-End developer, adept in web development. I am ready to take a position in a company with a friendly environment that will expand my learning and support my developer skills.</p>
-                        <p>Unfortunately, I don’t have work experience in web development yet, but I
-                            volunteer at events where I interact with people, so I have experience in team
-                            communication and can easily adapt to those around me, which is very important
-                            for a successful company that wants maximum productivity.  I am looking for a
-                            place to start my career in programming, learn something new and become a
-                            professional.</p>
+                        <p>5 months of work on private projects, you can find some of them in this site</p>
                         <h2>Education</h2>
                         <hr></hr>
                         <p className="course">High school at Merkinės Vinco Krėvės gimnasium, Merkinė<br /><span>SEPTEMBER 2006 — JULY 2019</span></p>
