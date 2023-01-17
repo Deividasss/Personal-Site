ae9d743737c58fa6ad63ee1e5e62cb9c35396c80
Deividasss <kasetukas123@gmail.com>
2023-01-17 18:23:09 +0200

    update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
                         <h1>Deividas <br /> Kašėta</h1>
                         <h3>Junior Web Developer</h3>
                         <p>Front-End developer, adept in web development. I am ready to take a position in a company with a friendly environment that will expand my learning and support my developer skills.</p>
-                        <p>5 months of work on private projects, you can find some of them in this site</p>
+                        <p>1 year of work on private projects, you can find some of them in this site</p>
                         <h2>Education</h2>
                         <hr></hr>
                         <p className="course">High school at Merkinės Vinco Krėvės gimnasium, Merkinė<br /><span>SEPTEMBER 2006 — JULY 2019</span></p>
