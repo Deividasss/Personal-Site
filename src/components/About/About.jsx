60f46607ef380dad9c6fdd398a10464cab7448e5
Deividasss <kasetukas123@gmail.com>
2023-01-17 18:45:42 +0200

    update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,8 +10,13 @@ const About = () => {
                     <div className="aboutHeading">
                         <h1>Deividas <br /> Kašėta</h1>
                         <h3>Junior Web Developer</h3>
-                        <p>Front-End developer, adept in web development. I am ready to take a position in a company with a friendly environment that will expand my learning and support my developer skills.</p>
-                        <p>1 year of work on private projects, you can find some of them in this site</p>
+                        <p>I believe that true craftsmanship is when 'design' and ' build' work together. Which is why when putting a site together I not only ask myself 'does it look good?', I'm also thinking 'will it work well?'. With me it's not all about image, It's about keeping a website on its toes and at the top of its game.</p>
+                        <p> I have 1 year of work on private projects. Every day I improve and try to learn new things, now I'm learning React-Native and
+                            I have a couple of months of practice in this field, so I could easily use it to develop
+                            your projects.</p>
+                        <p>In the future, I would like to become a full stack programmer and learn C# or
+                            Python to expand my knowledge and become a versatile worker who can handle
+                            more than just Front-End tasks</p>
                         <h2>Education</h2>
                         <hr></hr>
                         <p className="course">High school at Merkinės Vinco Krėvės gimnasium, Merkinė<br /><span>SEPTEMBER 2006 — JULY 2019</span></p>
