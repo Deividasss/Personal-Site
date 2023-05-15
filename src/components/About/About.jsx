74f0c92a071f2ed18542b9681959506f333cde79
Deividasss <kasetukas123@gmail.com>
2023-05-15 16:15:15 +0300

    update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,18 +11,15 @@ const About = () => {
                         <h1>Deividas <br /> Kašėta</h1>
                         <h3>Junior Web Developer</h3>
                         <p>I believe that true craftsmanship is when 'design' and ' build' work together. Which is why when putting a site together I not only ask myself 'does it look good?', I'm also thinking 'will it work well?'. With me it's not all about image, It's about keeping a website on its toes and at the top of its game.</p>
-                        <p> I have 1 year of work on private projects. Every day I improve and try to learn new things, now I'm learning React-Native and
-                            I have a couple of months of practice in this field, so I could easily use it to develop
-                            your projects.</p>
-                        <p>In the future, I would like to become a full stack programmer and learn C# or
-                            Python to expand my knowledge and become a versatile worker who can handle
-                            more than just Front-End tasks</p>
+                        <p> I am a Junior Front-End developer with more than one year of experience in the field. When I'm programming, I feel like I'm going for a walk in nature, because it's work that makes me relax. I started my programming path at the Baltic Institute of technology, where I learned the basics, after some time I entered the Present Connection academy, where I continued to deepen my skills. Currently I’m learning PHP and mySQL. I am looking for a company that will become my new programming home and help me continue to achieve my goals objectives.</p>
+                        
                         <h2>Education</h2>
                         <hr></hr>
                         <p className="course">High school at Merkinės Vinco Krėvės gimnasium, Merkinė<br /><span>SEPTEMBER 2006 — JULY 2019</span></p>
                         <h2>Courses</h2>
                         <hr></hr>
                         <p className="course">Front-End Web development at Baltic Institute Of Technology<br /><span>JANUARY 2022 — JUNE 2022</span></p>
+                        <p className="course"> Present Connection Full Stack Academy<br /><span>OCTOBER 2022 — FEBRUARY 2023</span></p>
                     </div>
                     <div className="aboutInfoImg">
                         <img className="aboutImg" src={mono1}></img>
@@ -34,6 +31,8 @@ const About = () => {
                             <p>MySQL</p>
                             <p>JavaScript</p>
                             <p>Git</p>
+                            <p>PHP</p>
+                            <p>WordPress</p>
                         </div>
                     </div>
                 </div>
